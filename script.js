bf68b92032dd0ddf66cd4b57d4196c93a8ca5fd0
Brock Young <bearfoxfish@gmail.com>
2023-07-09 10:42:11 -0700

    add containers

---
+++ script.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read;
+}
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read." : "not read yet."}`;
+}
+
+function addBookToLibrary() {
+
+}
