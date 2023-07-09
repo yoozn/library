d72e62be1a70cf2e4cda66c9af7d82a08f1d81d3
Brock Young <bearfoxfish@gmail.com>
2023-07-09 11:26:47 -0700

    added opening and closing of form

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const addEntry = document.querySelector(".add-entry");
+const formClose = document.querySelector(".close-button");
+const formContainer = document.querySelector(".form-container");
+const form = document.querySelector(".form");
+const libraryContainer = document.querySelector(".library-container");
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -14,3 +20,15 @@ Book.prototype.info = function() {
 function addBookToLibrary() {
 
 }
+
+addEntry.addEventListener('click', ()=> {
+    libraryContainer.style.visibility = "hidden";
+    formContainer.style.visibility = "visible";
+})
+
+formClose.addEventListener('click', (e) => {
+    form.reset();
+    libraryContainer.style.visibility = "visible";
+    formContainer.style.visibility = "hidden";
+})
+
