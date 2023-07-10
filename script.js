69d01af588c014ca32ad5ce9c1b54d6a829f5812
Brock Young <bearfoxfish@gmail.com>
2023-07-10 08:31:22 -0700

    refactor to include objects and arrays

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const formPages = document.querySelector("#pages");
 const formRead = document.querySelector("#read");
 const confirmEntry = document.querySelector(".submit");
 const libraryContainer = document.querySelector(".library-container");
-const libraryGrid = document.querySelector(".library-grid");
+const libraryInnerContainer = document.querySelector(".library");
+let libraryGrid = document.querySelector(".library-grid");
 
 let myLibrary = [];
 
@@ -24,43 +25,56 @@ Book.prototype.info = function() {
 }
 
 function addBookToLibrary() {
-
+    const newBook = new Book(formTitle.value, formAuthor.value, formPages.value, formRead.value);
+    myLibrary.push(newBook);
+    libraryGrid.remove();
+    libraryGrid = document.createElement('div');
+    libraryGrid.classList.add('library-grid');
+    libraryInnerContainer.appendChild(libraryGrid);
+    for (let i=0 ; i< myLibrary.length; i++) {
+        const book = document.createElement('div');
+        book.classList.add("book");
+        libraryGrid.appendChild(book);
+        const title = document.createElement('p');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+        const read = document.createElement('p');
+        title.classList.add('b-title');
+        author.classList.add('b-author');
+        pages.classList.add('b-pages');
+        read.classList.add('b-read');
+        title.textContent = myLibrary[i].title;
+        author.textContent = myLibrary[i].author;
+        pages.textContent = myLibrary[i].pages;
+        read.textContent = (myLibrary[i].read == "on") ? "read" : "not read";
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(pages);
+        book.appendChild(read);
+    }
 }
 
-addEntry.addEventListener('click', ()=> {
-    libraryContainer.style.visibility = "hidden";
-    formContainer.style.visibility = "visible";
-})
+function initialize() {
+    addEntry.addEventListener('click', ()=> {
+        libraryContainer.style.visibility = "hidden";
+        formContainer.style.visibility = "visible";
+    })
+    
+    formClose.addEventListener('click', (e) => {
+        form.reset();
+        libraryContainer.style.visibility = "visible";
+        formContainer.style.visibility = "hidden";
+    })
+    
+    confirmEntry.addEventListener('click', (e) => {
+        e.preventDefault();
+        addBookToLibrary();
+        libraryContainer.style.visibility = "visible";
+        formContainer.style.visibility = "hidden";
+    })
+}
 
-formClose.addEventListener('click', (e) => {
-    form.reset();
-    libraryContainer.style.visibility = "visible";
-    formContainer.style.visibility = "hidden";
-})
+initialize();
 
-confirmEntry.addEventListener('click', (e) => {
-    e.preventDefault();
-    const book = document.createElement('div');
-    book.classList.add("book");
-    libraryGrid.appendChild(book);
-    const title = document.createElement('p');
-    const author = document.createElement('p');
-    const pages = document.createElement('p');
-    const read = document.createElement('p');
-    title.classList.add('b-title');
-    author.classList.add('b-author');
-    pages.classList.add('b-pages');
-    read.classList.add('b-read');
-    title.textContent = formTitle.value;
-    author.textContent = formAuthor.value;
-    pages.textContent = formPages.value;
-    read.textContent = "on" ? "read" : "not read";
-    book.appendChild(title);
-    book.appendChild(author);
-    book.appendChild(pages);
-    book.appendChild(read);
-    libraryContainer.style.visibility = "visible";
-    formContainer.style.visibility = "hidden";
-})
 
 
